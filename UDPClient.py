3acd0fcdd445793b825d29d46c8abe12a300fa05
Yisely <ybarraz3@asu.edu>
2022-09-29 15:51:42 -0700

    Implemented register/exit
    UDPClient is now able to register with server

--- UDPClient.py
+++ UDPClient.py
@@ -5,18 +5,6 @@ import random
 ClientSocket = socket.socket()
 host = '10.120.70.106'
 port = 16001
-inGame = 0 #0 for not in game, 1 for player, 2 for dealer
-cards = []
-visibleCards = 2
-stock = ['A-D','2-D','3-D','4-D','5-D','6-D','7-D','8-D','9-D','10-D','J-D','Q-D','K-D',
-    'A-H','2-H','3-H','4-H','5-H','6-H','7-H','8-H','9-H','10-H','J-H','Q-H','K-H',
-    'A-S','2-S','3-S','4-S','5-S','6-S','7-S','8-S','9-S','10-S','J-S','Q-S','K-S',
-    'A-C','2-C','3-C','4-C','5-C','6-C','7-C','8-C','9-C','10-C','J-C','Q-C','K-C']
-random.shuffle(stock)                                 
-discard = []
-discard.append(stock[0])
-stock.pop(0)
-points = 0
 
 print('Waiting for connection')
 try:
@@ -27,111 +15,43 @@ except socket.error as e:
 ans = 'open'
 
 Response = ClientSocket.recv(1024)
-while ans != 'exit':
+while ans[0:5] != 'exit':
     ans = input('\nEnter a command:')
     #will check if any of the commands were used
-    if ans == 'query games' or ans == 'query players':
+    if ans[0:9] =='register ':
+        #register @<handle> <IPv4-address> <port> 
         ClientSocket.send(str.encode(ans))
         Response = ClientSocket.recv(1024)
         print(Response.decode('utf-8'))
-    elif ans[0:12] == 'de-register ':
+    elif ans == 'query handles':
+        #query handles
         ClientSocket.send(str.encode(ans))
         Response = ClientSocket.recv(1024)
         print(Response.decode('utf-8'))
-    elif ans[0:9] =='register ':
+    elif ans[0:7] == 'follow ':
+        #follow @<handle1> @<handle2>
         ClientSocket.send(str.encode(ans))
         Response = ClientSocket.recv(1024)
         print(Response.decode('utf-8'))
-    elif ans[0:4] == 'end ':
+    elif ans[0:5] == 'drop ':
+        #drop @<handle1> @<handle2>
         ClientSocket.send(str.encode(ans))
         Response = ClientSocket.recv(1024)
         print(Response.decode('utf-8'))
-    elif ans[0:11] == 'start game ':
+    elif ans[0:6] == 'tweet ':
+        #tweet @<handle> "tweet"
         ClientSocket.send(str.encode(ans))
         Response = ClientSocket.recv(1024)
         print(Response.decode('utf-8'))
-        if(Response == 'SUCCESS'):
-            inGame = 2
-    elif ans == "join game":
-        #if not currently in game
+    elif ans[0:10] == 'end-tweet ':
+        #tweet @<handle>
         ClientSocket.send(str.encode(ans))
         Response = ClientSocket.recv(1024)
         print(Response.decode('utf-8'))
-        inGame = Response
-    elif inGame == 1 or inGame == 2:#player
-        print('Welcome to game')
-        while inGame == 1 or inGame == 2:
-            response = ClientSocket.recv(1024)
-            decodedResp = response.decode('utf-8')
-            for i in cards:
-                #display if visible
-                if i == 2:
-                    print('\n')
-                if i < visibleCards:
-                    #print
-                    print(cards[i])
-                else:
-                    print(' *** ')
-                #do not display and instead print ***
-            if visibleCards == 6:
-                print('All card are turned, turn is passed')
-                if(inGame == 2):
-                    ClientSocket.send(str.encode('end'))
-            elif decodedResp[0:10] == 'recvcards ':
-                decodedResp.split(' ')#add cards to curr card list
-                decodedResp.remove('recvcards')
-                cards = decodedResp
-            elif decodedResp == 'your turn':
-                ans = input('\nEnter game command: ')#ask user for their input
-                cmdsend = ans + cards[5]#face down card if any
-                cards.pop(5)
-                ClientSocket.send(str.encode(cmdsend))#send the command
-                visibleCards += 1
-                if ans == 'stock':
-                    response = ClientSocket.recv(1024)
-                    decodedResp = response.decode('utf-8')
-                    cards.insert[visibleCards-1,decodedResp]
-                elif ans[0:6] == 'steal ':
-                    response = ClientSocket.recv(1024)
-                    decodedResp = response.decode('utf-8')
-                    cards.insert[visibleCards-1,decodedResp]
-                elif ans == 'discard':
-                    response = ClientSocket.recv(1024)
-                    decodedResp = response.decode('utf-8')
-                    cards.insert[visibleCards-1,decodedResp]
-            elif decodedResp[0:10] == 'stockCard ':
-                #dealer gets sent card to put in discard and sends card from stock
-                newdis = decodedResp.split(' ')
-                newdis.remove('stockCard')
-                discard.insert[0,newdis]
-                stkcrd = stock[0]
-                stock.pop(0)
-                ClientSocket.send(str.encode(stkcrd))
-            elif decodedResp[0:12] == 'discardCard ':
-                #dealer gets sent card to put in discard and sends card from discard
-                newdis = decodedResp.split(' ')
-                newdis.remove('discardCard')
-                discard.insert[0,newdis]
-                discrd = discard[0]
-                discard.pop(0)
-                ClientSocket.send(str.encode(discrd))
-            elif decodedResp == 'sixcard':#dealer sends starting six card
-                newcrd = 'recvcards'
-                for x in range(0, 6):
-                    newcrd = newcrd + ' ' + cards[x]
-                ClientSocket.send(str.encode(newcrd))
-            elif decodedResp[0:10] == 'stealcard ':#someone is stealing your card
-                #give your card and replace with the card in steal card
-                newcard = decodedResp.split(' ')
-                newcard.remove('stealcard')
-                sendcard = cards[0]
-                cards.pop(0)
-                cards.insert[0,newcard]
-                ClientSocket.send(str.encode(sendcard))
-            elif decodedResp == 'end'or decodedResp == 'exit':
-                break
-        
     elif ans != 'exit':
         print('not a valid command try again')
 
+ClientSocket.send(str.encode(ans))
+Response = ClientSocket.recv(1024)
+print(Response.decode('utf-8'))
 ClientSocket.close()
--- UDPServer.py
+++ UDPServer.py
@@ -5,8 +5,6 @@ from _thread import *
 ServerSocket = socket.socket()
 host = '10.120.70.106'
 port = 16001
-players = [] #user, IPv4, port, inGame: 0=no 1=yes&player 2=yes&dealer
-games = []#user, k, gameId
 clients = []#client, IPv4
 gameId = 100
 
@@ -18,106 +16,14 @@ except socket.error as e:
 print('Waitiing for a Connection..')
 ServerSocket.listen(5)
 
-def sortPlayers(list):
-    return list[3]
-
-def threadded_game(playerList, dealer, gameIdnum):
-    print('Started a new game')
-    #make sure players get marked as players
-    for i in players:
-        for j in playerList: # has the same thing as players but player[4] has client
-            if(j[0] == i[0]):
-                i[3] = 1
-    
-    #add the dealer to the player list to loop through
-    playerList.insert(0,dealer)
-
-    #add a special connection for dealer messages
-    dealerClient
-    for i in clients:
-        if i[1] == dealer[1]:
-            dealerClient = i[0]
-
-    #initialize cards
-    for i in playerList:
-        for j in clients:
-            if i[1] == j[1]:
-                i.append(j[0])
-                j[i].sendall(str.encode('player'))
-                msg = 'sixCard'#command to recieve starting cards
-                dealerClient.sendall(str.encode(msg))#sends msg
-                msgRecv = dealerClient.recv(2048)#recieve card from dealer
-                decodedmsg = msgRecv.decode('utf-8')#decode msg
-                j[i].sendall(str.encode(decodedmsg))#send the player their cards
-
-    #loop through turns
-    turn = 0
-    game = True
-    while game:
-        for currPlayer in playerList:
-            #maybe add a loop to ask user turn for input
-            msg = 'your turn'
-            currPlayer[4].sendall(str.encode(msg))#sends msg
-            msgRecv = currPlayer[4].recv(2048)#recieve player command
-            decodedmsg = msgRecv.decode('utf-8')#decode msg
-            if decodedmsg[0:6] == 'stock ':#stock cardToBeExchanged
-                #request a stock card from dealer
-                card = decodedmsg.split(' ')#split the message into the stock and card
-                newmsg = 'stockCard ' + card[1]#add the card to message
-                dealerClient.sendall(str.encode(newmsg))#send the command and card to dealer
-                msgRecv = dealerClient.recv(2048)#recieve new card from dealer
-                decodedmsg = msgRecv.decode('utc-8')#decode msg
-                currPlayer[4].sendall(str.encode(decodedmsg))#send card to player
-            elif decodedmsg[0:8] == 'discard ':
-                #request the discarded card
-                card = decodedmsg.split(' ')#split the message into the discard and card
-                newmsg = 'discardCard ' + card[1]
-                dealerClient.sendall(str.encode(newmsg))#send the command and card to dealer
-                msgRecv = dealerClient.recv(2048)#recieve new card from dealer
-                decodedmsg = msgRecv.decode('utc-8')#decode msg
-                currPlayer[4].sendall(str.encode(decodedmsg))#send card to player
-            elif decodedmsg[0:5] == "steal ":#steal the card from the specified player
-                card = decodedmsg.split(' ')#split msg into steal name and card
-                newmsg = 'stealcard ' + card[2]
-                for i in playerList:#send msg to player with card to swap
-                    if i[0] == card[1]:
-                        i[4].sendall(str.encode(newmsg))#send msg
-                        msgRecv = i[4].recv(2048)#receive new card from other player
-                        decodedmsg = msgRecv.decode('utc-8')
-                        currPlayer[4].sendall(str.encode(decodedmsg))#send the card back to og player
-            elif decodedmsg[0:4] == 'end ':#checks if command used was end game
-                reply = 'FAILURE'
-                initialLength = len(games)
-                games = [i for i in games if i[0] != decodedmsg[12:]]
-                if initialLength != len(games):
-                    reply = 'SUCCESS'
-                    game = False
-
-    #make sure playerList players get set to 0
-
 def threaded_client(connection):
-    connection.send(str.encode('Welcome to the Server!'))
+    connection.send(str.encode('Welcome to Tweeter!'))
     connec = True
     while connec:
         data = connection.recv(2048)
         decodeddata = data.decode('utf-8')
         reply = 'Server Says: ' + data.decode('utf-8')
-        if decodeddata == 'query games':#checks if command used was query games
-            if games:
-                reply = str(len(games)) + '\n' #the ammount of players
-                reply += str(games)#returns list
-            else:
-                reply = '0\n[]'#no games
-            connection.sendall(str.encode(reply))
-        elif decodeddata == 'query players':#checks if command used was query players
-            global players
-            if players:
-                reply = str(len(players)) + '\n' #the ammount of players
-                reply += str(players)#returns list
-            else:
-                reply = '0\n[]'#no players
-            connection.sendall(str.encode(reply))
-        elif decodeddata[0:9] == 'register ':#checks if command used was register
+        if decodeddata[0:9] == 'register ':#checks if command used was register
             player = decodeddata.split(' ')
             player.remove('register')
             player.append('0') # this number will represent 1 if in game 2 if dealer and 0 if not in game
@@ -132,56 +38,6 @@ def threaded_client(connection):
             if reply != 'FAILURE':
                 players.append(player)
                 reply = 'SUCCESS'
-            connection.sendall(str.encode(reply))
-        elif decodeddata[0:12] == 'de-register ':#checks if command used was de-register
-            reply = 'FAILURE'
-            initialLength = len(players)
-            players = [i for i in players if i[0] != decodeddata[12:]]
-            resultLength = len(players)
-            if initialLength != resultLength:
-                reply = 'SUCCESS'
-            connection.sendall(str.encode(reply))
-        elif decodeddata == 'join game':
-            reply = 0
-            for i in players:
-                if i[1] == address[0]:
-                    reply == i[3]
-            connection.sendall(str.encode(reply))
-        elif decodeddata == 'exit':
-            connec = False
-        elif decodeddata[0:11] == 'start game ':
-            reply = 'FAILURE'
-            game = decodeddata.split(' ')
-            game.remove('start')
-            game.remove('game') # list becomes: user k
-            gameStart = False
-            dealer = []
-            if 1 <= int(game[1]) <= 3:
-                for i in players:# check if user is in player
-                    if game[0] == player[0]:
-                        j = 0
-                        for i in players:#check if there are dealer is already in game
-                            if i[0] == game[0]:
-                                gameStart = True
-                                i[3] = 2
-                                dealer.append(i)
-                        for i in players:#check if there are sufficient players available
-                            if i[3] == '0':
-                                j = j + 1
-                        if j >= game[1]:
-                            if gameStart == True: #start a new thread and begin game
-                                players.sort(key=sortPlayers)
-                                playerList = players[0:j]
-                                gameId += 1
-                                start_new_thread(threadded_game(playerList,dealer,gameId))
-                                game.append(gameId)
-                                games.append(game)
-                                reply = 'SUCCESS'
-                        else:
-                            for i in players:#no game, set dealer back to 0
-                                if i[0] == game[0]:
-                                    gameStart = False
-                                    i[3] = 0
             connection.sendall(str.encode(reply))
         else:
             reply = 'error'
